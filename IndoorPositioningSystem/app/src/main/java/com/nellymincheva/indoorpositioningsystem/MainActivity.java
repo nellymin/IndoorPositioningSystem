44dfba8ac073abae924ed93f65dda2ae2523d3f8
Nelly Mincheva <nelly.m.mincheva@gmail.com>
2016-10-26 09:29:06 +0300

    Test service added

--- IndoorPositioningSystem/app/src/main/java/com/nellymincheva/indoorpositioningsystem/MainActivity.java
+++ IndoorPositioningSystem/app/src/main/java/com/nellymincheva/indoorpositioningsystem/MainActivity.java
@@ -1,9 +1,17 @@
 package com.nellymincheva.indoorpositioningsystem;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +19,40 @@ import android.widget.Button;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    Messenger mBeaconsService = null;
+    boolean mBeaconsBound;
+    /**
+     * Class for interacting with the main interface of the service.
+     */
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the object we can use to
+            // interact with the service.  We are communicating with the
+            // service using a Messenger, so here we get a client-side
+            // representation of that from the raw IBinder object.
+            mBeaconsService = new Messenger(service);
+            mBeaconsBound = true;
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mBeaconsService = null;
+            mBeaconsBound = false;
+        }
+    };
+
+    public void sayHello(View v) {
+        if (!mBeaconsBound) return;
+        // Create and send a message to the service, using a supported 'what' value
+        Message msg = Message.obtain(null, BeaconsService.MSG_SAY_HELLO, 0, 0);
+        try {
+            mBeaconsService.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +74,26 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to the service
+        bindService(new Intent(this, BeaconsService.class), mConnection,
+                Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Unbind from the service
+        if (mBeaconsBound) {
+            unbindService(mConnection);
+            mBeaconsBound = false;
+        }
+    }
+
+
 
     private void SwitchLanguage(String language) {
         Locale locale = new Locale(language);
